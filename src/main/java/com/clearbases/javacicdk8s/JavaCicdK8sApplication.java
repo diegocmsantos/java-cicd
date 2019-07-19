df084cd3ccb5c989d1a8cc4acd227e10697c050b
Diego Maia <diegocmsantos@gmail.com>
2019-07-19 15:35:31 -0300

    first commit

---
+++ src/main/java/com/clearbases/javacicdk8s/JavaCicdK8sApplication.java
@@ -0,0 +1,13 @@
+package com.clearbases.javacicdk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCicdK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCicdK8sApplication.class, args);
+	}
+
+}
